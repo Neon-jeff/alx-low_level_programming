b85216e5b5a156d207f9a393b3c22ad72a0059c6
Neon-jeff <omaksteejay@gmail.com>
2022-07-03 01:30:35 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
-*main entry point
+*Main- Entry point
 *
-*returns(0) for success
+*Returns : Always 0 (Success)
 */
 int main(void)
 {
-puts( "\"Programming is like building a multilingual puzzle" );
-return(0);
+puts ("\"Programming is like building a multilingual puzzle");
+return (0);
 }
